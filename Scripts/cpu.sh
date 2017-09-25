16779af195f2d5f5cc613583b02482e3b7dd1425
elena <lolotafrantzi@gmail.com>
2017-09-25 14:19:49 +0300

    updated cpu and weather scripts
    Former-commit-id: aa039acc39d32c2d39f2a7fbcb3c441729601dc3
    Former-commit-id: 511df177d316d7111196e5ddac6cd837a1c12be0
    Former-commit-id: 26ec14ec4e464d6978f6d6e2be43b7d24fc64422

--- Scripts/cpu.sh
+++ Scripts/cpu.sh
@@ -2,10 +2,17 @@
 
 #   
 CPU="$[100-$(vmstat 1 2|tail -1|awk '{printf "%d", $15}')]"
-GOVERNOR="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
+# GOVERNOR="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
 
-if [[ "$GOVERNOR" == "performance" ]]; then
+# if [[ "$GOVERNOR" == "performance" ]]; then
+# 	echo " $CPU%"
+# else
+# 	echo " $CPU%"
+# fi
+
+# Lazy whitespace padding
+if [ "$CPU" -ge "10" ]; then
 	echo " $CPU%"
 else
-	echo " $CPU%"
+	echo "  $CPU%"
 fi
