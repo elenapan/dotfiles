6fb5d00b1609628b4d5c530fa8413adc44f65fd0
elena <lolotafrantzi@gmail.com>
2017-09-26 12:25:53 +0300

    fixed url to show current weather instead of tomorrows weather (oops)

--- Scripts/accuweather.sh
+++ Scripts/accuweather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-weatherURL='http://www.accuweather.com/en/us/buffalo-ny/78701/weather-forecast/351193'
-TEMP="$(wget -q -O- "$weatherURL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°F" }'| head -1)"
+URL='https://www.accuweather.com/en/us/buffalo-ny/14202/weather-forecast/349726'
+TEMP="$(wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°F" }'| head -1)"
 echo " $TEMP"
