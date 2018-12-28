7ff4ba8ab4e49d2bf78cad2824bece32573fa701
elena <lolotafrantzi@gmail.com>
2018-12-28 19:44:24 +0200

    updated volume script to send notifications if the sidebar is not visible
    Former-commit-id: 9035303194af712c8810c56726e9fc2d4978fb98
    Former-commit-id: f934ef99a51191c0f0a894615d778e236542b59b
    Former-commit-id: c9445527a3ec7c8f37433f312152646c6d82374b
    Former-commit-id: c951f6268f8f02dd936424d5cbd65c99c800717a

--- bin/volume-control.sh
+++ bin/volume-control.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Control volume
 # Requires pulseaudio
+# AwesomeWM: Only sends a notification if the sidebar is not visible
 # --------------------------
 
 # Steps for raising/lowering volume
 STEP=5
 BIG_STEP=25
+
+# Notification config
+VOLUME_ICON=~/.config/awesome/themes/lovelace/icons/volume.png
+MUTED_ICON=~/.config/awesome/themes/lovelace/icons/muted.png
+NOTIF_ID=/tmp/volume-daemon-notification
 
 if [[ "$1" = "up" ]]; then
     pactl set-sink-mute 0 0 && pactl set-sink-volume @DEFAULT_SINK@ +$STEP%
@@ -17,6 +23,28 @@ elif [[ "$1" = "DOWN" ]]; then
     pactl set-sink-mute 0 0 && pactl set-sink-volume @DEFAULT_SINK@ -$BIG_STEP%
 elif [[ "$1" = "toggle" ]]; then
     pactl set-sink-mute 0 toggle
+elif [[ "$1" = "reset" ]]; then
+    pactl set-sink-mute 0 0 && pactl set-sink-volume @DEFAULT_SINK@ 50%
 else
     echo "No argument."
+fi
+
+SIDEBAR_VISIBLE="$(awesome-client 'return sidebar.visible' | awk '{print $2}')"
+echo $SIDEBAR_VISIBLE
+if [[ "$SIDEBAR_VISIBLE" == "false" ]]; then
+    var=$(pactl list sinks)
+
+    case $var in
+        *'Mute: yes'*)
+            out="off"
+            ICON=$MUTED_ICON
+            ;;
+        *)
+            # we want word splitting
+            set -- ${var#*Volume:}
+            out="$a$t$4"
+            ICON=$VOLUME_ICON
+    esac
+    #echo "Volume: $out"
+    notify-send --urgency low -R "$NOTIF_ID" -i $ICON "$out"
 fi
