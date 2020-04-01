0c156c605f1cbf21826bc77a7251108f76300fed
elenapan <lolotafrantzi@gmail.com>
2020-04-01 14:24:39 +0300

    Add note about daemons that need internet access

--- config/awesome/evil/init.lua
+++ config/awesome/evil/init.lua
@@ -12,5 +12,11 @@ require("evil.brightness")
 -- require("evil.spotify")
 
 -- Internet access required
+-- Note: These daemons use a temp file to store the retrieved
+-- values in order to check its modification time and decide if it is time to update or not.
+-- If you want to change their parameters (e.g.
+-- coronavirus_country), make sure to delete files
+-- `/tmp/awesomewm-evil-weather` and
+-- `/tmp/awesomewm-evil-coronavirus` before restarting AwesomeWM.
 require("evil.coronavirus")
 require("evil.weather")
