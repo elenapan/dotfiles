20f3fcab06ee3d70389f86bd9613ddd37a65cb36
elenapan <lolotafrantzi@gmail.com>
2020-06-20 17:12:21 +0300

    Remove useless code for applying theme variables to notifications
    It was required in older versions of Awesome, but not anymore

--- config/awesome/notifications/init.lua
+++ config/awesome/notifications/init.lua
@@ -13,43 +13,6 @@ naughty.config.defaults.timeout = 5
 naughty.config.presets.low.timeout = 2
 naughty.config.presets.critical.timeout = 12
 
--- Apply theme variables
-naughty.config.padding = beautiful.notification_padding
-naughty.config.spacing = beautiful.notification_spacing
-naughty.config.defaults.margin = beautiful.notification_margin
-naughty.config.defaults.border_width = beautiful.notification_border_width
-
-naughty.config.presets.normal = {
-    font         = beautiful.notification_font,
-    fg           = beautiful.notification_fg,
-    -- bg           = beautiful.notification_bg,
-    border_width = beautiful.notification_border_width,
-    margin       = beautiful.notification_margin,
-    -- position     = beautiful.notification_position
-}
-
-naughty.config.presets.low = {
-    font         = beautiful.notification_font,
-    fg           = beautiful.notification_fg,
-    -- bg           = beautiful.notification_bg,
-    border_width = beautiful.notification_border_width,
-    margin       = beautiful.notification_margin,
-    -- position     = beautiful.notification_position
-}
-
-naughty.config.presets.ok = naughty.config.presets.low
-naughty.config.presets.info = naughty.config.presets.low
-naughty.config.presets.warn = naughty.config.presets.normal
-
-naughty.config.presets.critical = {
-    font         = beautiful.notification_font,
-    fg           = beautiful.notification_crit_fg,
-    -- bg           = beautiful.notification_crit_bg,
-    border_width = beautiful.notification_border_width,
-    margin       = beautiful.notification_margin,
-    -- position     = beautiful.notification_position
-}
-
 -- Icon size
 -- naughty.config.defaults['icon_size'] = beautiful.notification_icon_size
 
