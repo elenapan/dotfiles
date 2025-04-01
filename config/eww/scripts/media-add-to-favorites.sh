fc678f176677acab3f751e10366e97e26f4f2b17
elenapan <elenapan@protonmail.com>
2025-04-01 16:50:14 +0300

    media: Optionally use rofi to save the current song in the chosen playlist

--- config/eww/scripts/media-add-to-favorites.sh
+++ config/eww/scripts/media-add-to-favorites.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
-# This script assumes you are saving your favorite songs in a text file like a
+# This script assumes you are saving your favorite songs in text files like a
 # caveman.
-favorites_file="$HOME/mus/playlists/songs"
+rofi="$HOME/.config/eww/scripts/rofi-with-mode-restore sidebar"
+playlists_dir="$HOME/mus/playlists"
+if [ "$1" == "select-playlist" ]; then
+    selected="$(find "$playlists_dir" -mindepth 1 -type f -exec basename {} \; | $rofi -dmenu -p "Select playlist to save to >")"
+    if [[ "$selected" == "" ]]; then
+        exit 1
+    fi
+else
+    selected="songs"
+fi
+
+full_path="$playlists_dir/$selected"
 
 # Get current media information displayed in eww
 eww_media_json="$(eww get media-json)"
@@ -9,6 +20,6 @@ artist="$(jq -r '.artist' <<< "$eww_media_json")"
 title="$(jq -r '.title' <<< "$eww_media_json")"
 
 line="${artist} - ${title}"
-echo "$line" >> "$favorites_file"
+echo "$line" >> "$full_path"
 
-notify-send.sh -u low -R /tmp/eww-media-favorites-notif "Added to favorites" "$line"
+notify-send.sh -u low -R /tmp/eww-media-favorites-notif "Added to $selected playlist" "$line"
